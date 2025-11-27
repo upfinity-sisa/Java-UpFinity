f6e8c09f77bcfd3ce458162782454bc7ae90593c
brenokas <brenofreitas28f@gmail.com>
2025-11-27 16:04:28 -0300

    update: colocando o ip da aws na conexao

--- src/main/java/school/sptech/Conexao.java
+++ src/main/java/school/sptech/Conexao.java
@@ -7,7 +7,7 @@ public class Conexao {
     public DriverManagerDataSource getConexao() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/upfinity?useSSL=false&serverTimezone=UTC");
+        dataSource.setUrl("jdbc:mysql://3.212.222.224:3306/upfinity?useSSL=false&serverTimezone=UTC");
         dataSource.setUsername("funcUpFinity");
         dataSource.setPassword("UpFinity123"); // ajuste conforme seu MySQL
         return dataSource;
